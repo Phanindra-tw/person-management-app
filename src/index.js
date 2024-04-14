104bf06b339da5310d1449d3f8f291b3e29d1475
Phanindra-tw <v.tadikonda@thoughtworks.com>
2024-04-15 00:55:19 +0530

    WIP Add main

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(
-  <App />,
-
-  document.getElementById('root')
-);
-registerServiceWorker();
-console.log('In here');
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './App';
+// import registerServiceWorker from './registerServiceWorker';
+//
+// ReactDOM.render(
+//   <App />,
+//
+//   document.getElementById('root')
+// );
+// registerServiceWorker();
+// console.log('In here');
 export { default as CreatePerson } from './containers/CreatePerson';
 export { default as PersonDashboard } from './containers/PersonDashboard';
 export { default as EditPerson } from './containers/EditPerson';
