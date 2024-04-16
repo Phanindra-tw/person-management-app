ea32c5e1c7ac18752559cd499683d90a62d83b0a
Phanindra-tw <v.tadikonda@thoughtworks.com>
2024-04-16 14:57:20 +0530

    Create component Home page

--- src/index.js
+++ src/index.js
@@ -1,16 +1,1 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import './index.css';
-// import App from './App';
-// import registerServiceWorker from './registerServiceWorker';
-//
-// ReactDOM.render(
-//   <App />,
-//
-//   document.getElementById('root')
-// );
-// registerServiceWorker();
-// console.log('In here');
-export { default as CreatePerson } from './containers/CreatePerson';
-export { default as PersonDashboard } from './containers/PersonDashboard';
-export { default as EditPerson } from './containers/EditPerson';
+export { default as HomePage } from './containers/HomePage';
